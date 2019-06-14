98b2840781cc3ba2b142d29c935fb085277f7d89
auyer <rafael@rcpassos.me>
2019-06-13 23:05:53 -0300

    Tensorboard, load weights, freze parameters

--- train_placesCNN.py
+++ train_placesCNN.py
@@ -17,8 +17,9 @@ import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 import torchvision.models as models
+from tensorboardX import SummaryWriter
+writer = SummaryWriter()
 
-import wideresnet
 import pdb
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
@@ -34,7 +35,7 @@ parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet18',
                         ' (default: resnet18)')
 parser.add_argument('-j', '--workers', default=6, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
-parser.add_argument('--epochs', default=90, type=int, metavar='N',
+parser.add_argument('--epochs', default=10, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
@@ -48,48 +49,54 @@ parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--print-freq', '-p', default=10, type=int,
                     metavar='N', help='print frequency (default: 10)')
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
 parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
                     help='evaluate model on validation set')
 parser.add_argument('--pretrained', dest='pretrained', action='store_false',
                     help='use pre-trained model')
-parser.add_argument('--num_classes',default=365, type=int, help='num of class in the model')
+parser.add_argument('--num_classes',default=2, type=int, help='num of class in the model')
 parser.add_argument('--dataset',default='places365',help='which dataset to train')
 
 best_prec1 = 0
-
+UNFREZE_LVL = 2
 
 def main():
-    global args, best_prec1
+    global args, best_prec1, globaliter_train, globaliter_val
+    globaliter_train = 0
+    globaliter_val = 0
     args = parser.parse_args()
     print(args)
     # create model
     print("=> creating model '{}'".format(args.arch))
-    if args.arch.lower().startswith('wideresnet'):
-        # a customized resnet model with last feature map size as 14x14 for better class activation mapping
-        model  = wideresnet.resnet50(num_classes=args.num_classes)
-    else:
-        model = models.__dict__[args.arch](num_classes=args.num_classes)
 
-    if args.arch.lower().startswith('alexnet') or args.arch.lower().startswith('vgg'):
+    model = models.__dict__[args.arch](num_classes=365)
+    model_file = '%s_places365.pth.tar' % args.arch
+    if not os.access(model_file, os.W_OK):
+        weight_url = 'http://places2.csail.mit.edu/models_places365/' + model_file
+        os.system('wget ' + weight_url)
+
+    checkpoint = torch.load(model_file, map_location=lambda storage, loc: storage)
+    state_dict = {str.replace(k,'module.',''): v for k,v in checkpoint['state_dict'].items()}
+    model.load_state_dict(state_dict)
+
+    
+    # Freze all pre-trained parametes
+    for param in model.parameters():
+        param.requires_grad = False
+    # Add unfrozen Fully connected Layer
+    model.fc = nn.Linear(512 , args.num_classes)        
+    
+    # Unfreze last block
+    if UNFREZE_LVL == 2: 
+        for param in model.layer4.parameters():
+            param.requires_grad = True
+
+    if args.arch.lower().startswith('vgg'):
         model.features = torch.nn.DataParallel(model.features)
         model.cuda()
     else:
         model = torch.nn.DataParallel(model).cuda()
+
     print(model)
-    # optionally resume from a checkpoint
-    if args.resume:
-        if os.path.isfile(args.resume):
-            print("=> loading checkpoint '{}'".format(args.resume))
-            checkpoint = torch.load(args.resume)
-            args.start_epoch = checkpoint['epoch']
-            best_prec1 = checkpoint['best_prec1']
-            model.load_state_dict(checkpoint['state_dict'])
-            print("=> loaded checkpoint '{}' (epoch {})"
-                  .format(args.resume, checkpoint['epoch']))
-        else:
-            print("=> no checkpoint found at '{}'".format(args.resume))
 
     cudnn.benchmark = True
 
@@ -119,10 +126,9 @@ def main():
         batch_size=args.batch_size, shuffle=False,
         num_workers=args.workers, pin_memory=True)
 
-    # define loss function (criterion) and pptimizer
+    # define loss function (criterion) and optimizer
     criterion = nn.CrossEntropyLoss().cuda()
-
-    optimizer = torch.optim.SGD(model.parameters(), args.lr,
+    optimizer = torch.optim.SGD( filter(lambda p: p.requires_grad, model.parameters()), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
 
@@ -132,7 +138,6 @@ def main():
 
     for epoch in range(args.start_epoch, args.epochs):
         adjust_learning_rate(optimizer, epoch)
-
         # train for one epoch
         train(train_loader, model, criterion, optimizer, epoch)
 
@@ -148,9 +153,13 @@ def main():
             'state_dict': model.state_dict(),
             'best_prec1': best_prec1,
         }, is_best, args.arch.lower())
+    
+    writer.export_scalars_to_json("./all_scalars.json")
+    writer.close()
 
 
 def train(train_loader, model, criterion, optimizer, epoch):
+    global globaliter_train
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
@@ -173,7 +182,7 @@ def train(train_loader, model, criterion, optimizer, epoch):
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 1))
         losses.update(loss.item(), input.size(0))
         top1.update(prec1.item(), input.size(0))
         top5.update(prec5.item(), input.size(0))
@@ -188,17 +197,20 @@ def train(train_loader, model, criterion, optimizer, epoch):
         end = time.time()
 
         if i % args.print_freq == 0:
+            globaliter_train += 1
             print('Epoch: [{0}][{1}/{2}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
                    epoch, i, len(train_loader), batch_time=batch_time,
-                   data_time=data_time, loss=losses, top1=top1, top5=top5))
+                   data_time=data_time, loss=losses, top1=top1))
+            writer.add_scalar('loss', loss.item(), globaliter_train/args.print_freq)
+            writer.add_scalar('Accuracy', top1.val, globaliter_train/args.print_freq)
 
 
 def validate(val_loader, model, criterion):
+    global globaliter_val
     batch_time = AverageMeter()
     losses = AverageMeter()
     top1 = AverageMeter()
@@ -218,7 +230,7 @@ def validate(val_loader, model, criterion):
         loss = criterion(output, target_var)
 
         # measure accuracy and record loss
-        prec1, prec5 = accuracy(output.data, target, topk=(1, 5))
+        prec1, prec5 = accuracy(output.data, target, topk=(1, 1))
         losses.update(loss.item(), input.size(0))
         top1.update(prec1.item(), input.size(0))
         top5.update(prec5.item(), input.size(0))
@@ -228,15 +240,17 @@ def validate(val_loader, model, criterion):
         end = time.time()
 
         if i % args.print_freq == 0:
+            globaliter_val += 1
             print('Test: [{0}/{1}]\t'
                   'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
                   'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                  'Prec@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
                    i, len(val_loader), batch_time=batch_time, loss=losses,
                    top1=top1, top5=top5))
-
-    print(' * Prec@1 {top1.avg:.3f} Prec@5 {top5.avg:.3f}'
+        
+            writer.add_scalar('Validation Loss', loss.item(), globaliter_val/args.print_freq)
+            writer.add_scalar('Validation Accuracy', top1.val, globaliter_val/args.print_freq)
+    print(' * Prec@1 {top1.avg:.3f} Prec@1 {top5.avg:.3f}'
           .format(top1=top1, top5=top5))
 
     return top1.avg
